6bafc36ab812b26ba88ffc819fd8e210176161e3
Oriana Bautista <oribautista99@gmail.com>
2025-10-06 10:02:32 -0400

    changes for deployment

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export async function GET() {
   const supabase = await createClient();
 
